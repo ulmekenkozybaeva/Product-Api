61d41826296fc6112c2c1ea26210392debb9fb23
Ulmeken Kozybaeva <ulyanakozibaeva@gmail.com>
2025-07-09 23:31:29 +0500

    Update models.py

--- myproject/products/models.py
+++ myproject/products/models.py
@@ -6,6 +6,7 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
