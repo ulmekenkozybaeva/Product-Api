85647996bc259bea40e16d125aa666b7f5204497
Ulmeken Kozybaeva <ulyanakozibaeva@gmail.com>
2025-07-09 23:32:11 +0500

    Update filters.py

--- myproject/products/filters.py
+++ myproject/products/filters.py
@@ -1,6 +1,7 @@
 import django_filters
 from .models import Product
 
+
 class ProductFilter(django_filters.FilterSet):
     price_min = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
     price_max = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
