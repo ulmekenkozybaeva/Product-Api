5d4b9714b7b5933c6afdee9b717f896c626a0215
Ulmeken Kozybaeva <ulyanakozibaeva@gmail.com>
2025-07-12 23:29:06 +0500

    Update models.py

--- myproject/products/models.py
+++ myproject/products/models.py
@@ -7,13 +7,12 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image_url = models.URLField(blank=True, null=True, verbose_name="Ссылка на изображение товара")
 
     def __str__(self):
         return self.name
-
