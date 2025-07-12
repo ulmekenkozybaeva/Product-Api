52f2f21bc908879905b175eb8529c3d7bd09388e
Ulmeken Kozybaeva <ulyanakozibaeva@gmail.com>
2025-07-12 23:28:40 +0500

    Update serializers.py

--- myproject/products/serializers.py
+++ myproject/products/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import Product, Category
 
-
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
@@ -12,20 +11,22 @@ class ProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'description', 'price', 'category']
+        fields = ['id', 'name', 'description', 'price', 'category', 'image_url']
 
 class ProductSerializer2(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=200)
     description = serializers.CharField(allow_blank=True, allow_null=True)
     price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    image_url = serializers.URLField(allow_blank=True, allow_null=True)
 
     def create(self, validated_data):
         return Product.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        instance.name = validated_data.get('title', instance.name)
+        instance.name = validated_data.get('name', instance.name)
         instance.description = validated_data.get('description', instance.description)
-        instance.price = validated_data.get('completed', instance.price)
+        instance.price = validated_data.get('price', instance.price)
+        instance.image_url = validated_data.get('image_url', instance.image_url)
         instance.save()
         return instance
